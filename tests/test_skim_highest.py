e074883c2a1a41cdab91d5020f839f33979ad89c
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-02-20 19:30:26 -0500

    Initial commit

---
+++ tests/test_skim_highest.py
@@ -0,0 +1,52 @@
+from pecking import skim_highest
+
+
+def test_skim_highest_two_groups_signif():
+    group1 = [1, 1, 1, 2, 2, 2, 3, 3, 3, 4]
+    group2 = [5, 5, 5, 6, 6, 6, 7, 7, 7, 8]
+
+    samples = [group1, group2]
+    labels = ["Lower", "Higher"]
+
+    assert skim_highest(samples, labels) == ["Higher"]
+
+
+def test_skim_highest_two_groups_insig():
+    group1 = [1, 1, 1, 2, 2, 2, 3, 3, 3, 4]
+    group2 = [2, 2, 2, 3, 3, 3, 4, 4, 4, 5]
+
+    samples = [group1, group2]
+
+    assert skim_highest(samples) == []
+
+
+def test_skim_highest_three_groups_signif1():
+    group1 = [1, 1, 1, 2, 2, 2, 3, 3, 3, 4]
+    group2 = [4, 4, 4, 5, 5, 5, 6, 6, 6, 7]
+    group3 = [7, 7, 7, 8, 8, 8, 9, 9, 9, 10]
+
+    samples = [group1, group2, group3]
+    labels = ["Bottom", "Middle", "Top"]
+
+    assert skim_highest(samples, labels) == ["Top"]
+
+
+def test_skim_highest_three_groups_signif2():
+    group1 = [1, 1, 4, 2, 2, 2, 3, 3, 3, 4]
+    group2 = [1, 1, 2, 2, 2, 2, 3, 4, 3, 4]
+    group3 = [8, 8, 8, 9, 9, 9, 10, 10, 10, 11]
+
+    samples = [group2, group3, group1]
+
+    assert skim_highest(samples) == [1]
+
+
+def test_skim_highest_three_groups_insig():
+    group1 = [1, 1, 4, 2, 2, 2, 3, 3, 3, 4]
+    group2 = [1, 1, 2, 2, 2, 2, 3, 4, 3, 4]
+    group3 = [1, 0, 2, 4, 2, 2, 3, 4, 3, 4]
+
+    samples = [group1, group2, group3]
+    labels = ["Bottom", "Middle", "Top"]
+
+    assert skim_highest(samples, labels) == []
